c3560665c0944d464fcac405b16d26fe6c08c453
Alexander Ilendzeier <66219127+LisTRK@users.noreply.github.com>
2026-03-10 20:53:50 +0200

    Delete src directory

--- src/js/faq.js
+++
@@ -1,6 +0,0 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
-new Accordion('.faq-list', {
-  showMultiple: true,
-});
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './js/footer.js'
-import './js/about';
-import './js/covers';
-import './js/header';
-import './js/projects';
-import './js/reviews';
-import './js/faq';
